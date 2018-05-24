bb176a4bb3af7b8dd4355e650a09b5b9c42f82d9
song <mssongan840136@outlook.com>
2018-05-24 11:41:27 +0900

    change message shown

--- js/layers/message.js
+++ js/layers/message.js
@@ -2,19 +2,17 @@ import {Status} from '../Timer.js';
 
 export function createMessageLayer(font, timer, playerEnv, level) {
   const LINE1 = 40;
-  const LINE2 = 115;
+  const LINE2 = 205;
 
   return function drawMessageLayer(context) {
     const {score, player} = playerEnv.playerController;
 
     if (timer.state.status === Status.READY) {
-      font.printLines('The chick was lost in city.\n' +
-      'Help him return home.\n' +
-      'But please pay attention to\n' +
+      font.printLines('You ara a chick, and you are\n' +
+      'lost in city. Return home.\n' +
+      'But please watch out for\n' +
       'the PIPES.', context, 16, LINE1);
-      font.printLines('Press SPACE to start game and\n' +
-      'control the bird to fly.', context, 16, LINE2);
-      font.print('Use SML to change chick\'s weight.', context, 10, LINE2 + 80);
+      font.printLines('Press SPACE to start game and fly.', context, 10, LINE2);
     } else if (timer.state.status === Status.PAUSED){
       font.printLines('PAUSED!', context, 115, LINE2);
     }
@@ -22,24 +20,24 @@ export function createMessageLayer(font, timer, playerEnv, level) {
     if (player.killable.dead) {
       if (player.SIZE == 'l') {
         font.printLines('Oh! You failed!\nYou are too fat!\nYou need more training!\
-          \nPress Enter to player again.',
+          \nPress Enter to play again.',
           context, 16, 80);
       } else if (player.SIZE == 'm') {
-        font.printLines('Oh! You failed!\nGet used to control the chick.\
-          \nPress Enter to player again.',
+        font.printLines('Oh! You failed!\nGet used to controlling the chick.\
+          \nPress Enter to play again.',
           context, 16, 80);
       } else if (player.SIZE == 's') {
         font.printLines('Oh! You failed!\nYou are too slim!\nMaybe you need to eat more!\
-          \nPress Enter to player again.',
+          \nPress Enter to play again.',
           context, 16, 80);
       }
     }
 
     if (score >= level.WIN) {
       font.printLines('Congratulations! You won!\n' +
-      'The chick comes back home,\n' +
-      'and so many chicks are\n' +
-      'flying together!', context, 30, 80);
+      'You came back home, and your\n' +
+      'friends ara all happy to see\n' +
+      'you!', context, 30, 80);
     }
   };
 }
